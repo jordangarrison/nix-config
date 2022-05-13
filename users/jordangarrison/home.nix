10eba419a179b9d82dcbe43102b58cd15f150191
Jordan Garrison <jordan.andrew.garrison@gmail.com>
2022-05-13 04:07:22 -0500

    updated doom emacs to use the url which is up to date.
    The other repo is no longer being maintained.

--- users/jordangarrison/home.nix
+++ users/jordangarrison/home.nix
@@ -2,7 +2,7 @@
 
 let
   doom-emacs = pkgs.callPackage (builtins.fetchTarball {
-    url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
+    url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
   }) {
     doomPrivateDir = ./tools/doom.d;
                              

23dd2aad2f06cce9426a550fb2006c6e7721afdb
Jordan Garrison <jordangarrison@users.noreply.github.com>
2023-09-16 20:14:01 +0000

    update to try xrdp, didnt work

--- workstation/configuration.nix
+++ workstation/configuration.nix
@@ -67,6 +67,15 @@ in
     desktopManager.gnome.enable = true;
   };
   services.gnome.gnome-keyring.enable = true;
+  services.gnome.gnome-remote-desktop.enable = true;
+
+  # services.xserver.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # services.xrdp.enable = true;
+  # services.xrdp.defaultWindowManager = "startplasma-x11";
+  # services.xrdp.openFirewall = true;
 
   # Env vars
   # environment.variables = {

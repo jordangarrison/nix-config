740351f895c4ccc5a0ec4f102b80b1d37c6fa19f
Jordan Garrison <jordangarrison@users.noreply.github.com>
2023-11-09 01:54:29 +0000

    added gptcommit to the repo, did not configure it

--- users/jordangarrison/home.nix
+++ users/jordangarrison/home.nix
@@ -2,23 +2,20 @@
 
 let
   unstable = import
-    (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz")
-    {
+    (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {
       config = config.nixpkgs.config;
     };
-in
-{
+in {
   nixpkgs.config.allowUnfree = true;
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username =
     if pkgs.stdenv.isLinux then "jordangarrison" else "jordan.garrison";
   # temporary hack for work
-  home.homeDirectory =
-    if pkgs.stdenv.isLinux then
-      "/home/jordangarrison"
-    else
-      "/Users/jordan.garrison";
+  home.homeDirectory = if pkgs.stdenv.isLinux then
+    "/home/jordangarrison"
+  else
+    "/Users/jordan.garrison";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -49,6 +46,7 @@ in
       # doom-emacs
 
       # Utilities
+      unstable.gptcommit
       unstable.helix
       unstable.lapce
       unstable.neovim

3500fa354be4e029794c63eea40fded250024d7a
Jordan Garrison <jordangarrison@users.noreply.github.com>
2025-02-18 01:10:39 -0600

    Clean up commented-out configurations in home.nix

--- users/jordangarrison/home.nix
+++ users/jordangarrison/home.nix
@@ -219,71 +219,6 @@ in
     '';
   };
 
-  # programs.neovim = {
-  #   package = unstable.neovim;
-  #   enable = true;
-  #   viAlias = true;
-  #   extraConfig = ''
-  #     luafile ${./tools/nvim/jag.lua}
-  #   '';
-  # };
-
-  # programs.tmux = {
-  #   enable = true;
-  #   aggressiveResize = true;
-  #   escapeTime = 0;
-  #   historyLimit = 9999;
-  #   terminal = "screen-256color";
-  #   keyMode = "vi";
-  #   extraConfig = ''
-  #     # Escape key please stop hating me
-  #     set -s escape-time 0
-
-  #     # Split panes easily
-  #     bind c new-window -c "#{pane_current_path}"
-  #     bind-key -r j run-shell "tmux neww ~/.local/bin/tmux-cht.sh"
-  #     bind -n M-'|' split-window -h -c "#{pane_current_path}"
-  #     bind -n M-'\' split-window -v -c "#{pane_current_path}"
-  #     unbind '"'
-  #     unbind %
-
-  #     # Easier nav
-  #     bind -n M-h select-pane -L
-  #     bind -n M-l select-pane -R
-  #     bind -n M-k select-pane -U
-  #     bind -n M-j select-pane -D
-
-  #     bind -n C-L next-window
-  #     bind -n C-H previous-window
-
-  #     bind -n M-H resize-pane -L 5
-  #     bind -n M-L resize-pane -R 5
-  #     bind -n M-K resize-pane -U 5
-  #     bind -n M-J resize-pane -D 5
-  #     bind -n M-M resize-pane -Z
-
-  #     # mouse mode
-  #     set -g mouse on
-
-  #     # Design
-  #     set -g status-position top
-  #     set -g visual-activity off
-  #     set -g visual-bell off
-  #     set -g visual-silence off
-  #     set -g bell-action none
-
-  #     # Last window
-  #     bind-key C-b last-window
-
-  #     # renumber windows
-  #     set-option -g renumber-windows on
-
-  #     # Plugins
-  #     run-shell ${pkgs.tmuxPlugins.resurrect.rtp}
-  #     run-shell ${pkgs.tmuxPlugins.continuum.rtp}
-  #   '';
-  # };
-
   programs.direnv = {
     enable = true;
     nix-direnv = {
@@ -315,20 +250,9 @@ in
     # Wezterm
     ".config/wezterm/wezterm.lua".source = ./tools/wezterm/wezterm.lua;
 
-    # K9s
-    # ".config/k9s/config.yml".source = ./tools/k9s/config.yml;
-    # ".config/k9s/skin.yml".source = ./tools/k9s/skin.yml;
-
-    # Btop
-    # ".config/btop/btop.conf".source = ./tools/btop/btop.conf.yml;
-
     # Scripts
     ".local/bin/tmux-cht.sh".source = ./tools/scripts/tmux-cht.sh;
     ".tmux-cht-languages".source = ./tools/scripts/tmux-cht-languages.txt;
     ".tmux-cht-commands".source = ./tools/scripts/tmux-cht-commands.txt;
-    # ".local/bin/okaws".source = ./tools/scripts/awsokta.sh;
-    # ".config/nix/nix.conf".text = ''
-    #   experimental-features = nix-command flakes
-    # '';
   };
 }

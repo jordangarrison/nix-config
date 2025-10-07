4f551f66185233b384ae863fd782e5b315af4e20
Jordan Garrison <jordangarrison@users.noreply.github.com>
2025-10-06 21:25:25 -0500

    fix(opportunity): Target caps2esc to laptop keyboard only
    Limit interception-tools caps2esc to the built-in Framework keyboard
    by adding NAME filter. This prevents the remapping from affecting
    external keyboards.

--- hosts/opportunity/configuration.nix
+++ hosts/opportunity/configuration.nix
@@ -37,6 +37,7 @@
     udevmonConfig = ''
       - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
         DEVICE:
+          NAME: "AT Translated Set 2 keyboard"
           EVENTS:
             EV_KEY: [KEY_CAPSLOCK, KEY_ESC, KEY_LEFTCTRL]
     '';

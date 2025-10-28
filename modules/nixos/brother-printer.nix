f7ee58a2011114e2f68d74f3ccb9b98221e6f1b8
Jordan Garrison <jordangarrison@users.noreply.github.com>
2025-10-27 22:40:10 -0500

    fix(printer): static ip auto allocated for printer

--- modules/nixos/brother-printer.nix
+++ modules/nixos/brother-printer.nix
@@ -10,4 +10,14 @@
     enable = true;
     extraBackends = with pkgs; [ sane-airscan ];
   };
+
+  hardware.printers = {
+    ensurePrinters = [{
+      name = "Brother_3765";
+      location = "Office";
+      deviceUri = "ipp://192.168.68.73/ipp/print";
+      model = "everywhere";
+    }];
+    ensureDefaultPrinter = "Brother_3765";
+  };
 }

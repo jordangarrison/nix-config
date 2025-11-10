0ad6b876aeeb4b42f50f2c1c9eacaf9c7916a617
Jordan Garrison <jordangarrison@users.noreply.github.com>
2025-11-10 15:38:29 -0600

    fix(flomac): Disable masApps due to mas-cli bug and add Slack
    - Comment out masApps configuration to work around mas-cli issue #1029
      causing PKInstallErrorDomain 201 errors on macOS 26.1+
    - Add slack to Homebrew casks list for manual installation
    - Preserve masApps configuration for re-enabling when mas 3.0.0 is released

--- hosts/flomac/configuration.nix
+++ hosts/flomac/configuration.nix
@@ -54,6 +54,7 @@
       "raycast"
       "readdle-spark"
       "rectangle"
+      "slack"
       "superwhisper"
       "todoist-app"
       "visual-studio-code"
@@ -63,46 +64,50 @@
       "windows-app"
       "zoom"
     ];
-    masApps = {
-      "1Blocker" = 1365531024;
-      "1Password for Safari" = 1569813296;
-      "Actions" = 1586435171;
-      "Amphetamine" = 937984704;
-      "Awesome Screenshot & Recorder" = 1531282066;
-      "Copilot" = 1447330651;
-      "Dark Reader for Safari" = 1438243180;
-      "Data Jar" = 1453273600;
-      "Exporter" = 1099120373;
-      "Flow" = 1423210932;
-      "GarageBand" = 682658836;
-      "HotKey" = 975890633;
-      "iMovie" = 408981434;
-      "JSONPeep" = 1458969831;
-      "Ka-Block!" = 1335413823;
-      "Keymapp" = 6472865291;
-      "Keynote" = 409183694;
-      "Kindle" = 302584613;
-      "Notability" = 360593530;
-      "Numbers" = 409203825;
-      "Okta Extension App" = 1439967473;
-      "OmniFocus" = 1542143627;
-      "Online Check" = 6504709660;
-      "Ooooo" = 1482773008;
-      "Pages" = 409201541;
-      "Perplexity" = 6714467650;
-      "S3" = 6447647340;
-      "Save to Reader" = 1640236961;
-      "Sequel Ace" = 1518036000;
-      "Shareful" = 1522267256;
-      "Shell Fish" = 1336634154;
-      "Streaks" = 963034692;
-      "Swift Playground" = 1496833156;
-      "Tailscale" = 1475387142;
-      "Taskheat" = 1431995750;
-      "Velja" = 1607635845;
-      "WhatsApp" = 310633997;
-      "Xcode" = 497799835;
-    };
+    # TODO: Re-enable masApps when mas 3.0.0 is released
+    # Currently disabled due to mas-cli bug #1029: PKInstallErrorDomain 201 errors
+    # on macOS 26.1+ preventing install/upgrade commands from working.
+    # See: https://github.com/mas-cli/mas/issues (Issue #1029)
+    # masApps = {
+    #   "1Blocker" = 1365531024;
+    #   "1Password for Safari" = 1569813296;
+    #   "Actions" = 1586435171;
+    #   "Amphetamine" = 937984704;
+    #   "Awesome Screenshot & Recorder" = 1531282066;
+    #   "Copilot" = 1447330651;
+    #   "Dark Reader for Safari" = 1438243180;
+    #   "Data Jar" = 1453273600;
+    #   "Exporter" = 1099120373;
+    #   "Flow" = 1423210932;
+    #   "GarageBand" = 682658836;
+    #   "HotKey" = 975890633;
+    #   "iMovie" = 408981434;
+    #   "JSONPeep" = 1458969831;
+    #   "Ka-Block!" = 1335413823;
+    #   "Keymapp" = 6472865291;
+    #   "Keynote" = 409183694;
+    #   "Kindle" = 302584613;
+    #   "Notability" = 360593530;
+    #   "Numbers" = 409203825;
+    #   "Okta Extension App" = 1439967473;
+    #   "OmniFocus" = 1542143627;
+    #   "Online Check" = 6504709660;
+    #   "Ooooo" = 1482773008;
+    #   "Pages" = 409201541;
+    #   "Perplexity" = 6714467650;
+    #   "S3" = 6447647340;
+    #   "Save to Reader" = 1640236961;
+    #   "Sequel Ace" = 1518036000;
+    #   "Shareful" = 1522267256;
+    #   "Shell Fish" = 1336634154;
+    #   "Streaks" = 963034692;
+    #   "Swift Playground" = 1496833156;
+    #   "Tailscale" = 1475387142;
+    #   "Taskheat" = 1431995750;
+    #   "Velja" = 1607635845;
+    #   "WhatsApp" = 310633997;
+    #   "Xcode" = 497799835;
+    # };
   };
 
   security.pam.services.sudo_local.touchIdAuth = true;
